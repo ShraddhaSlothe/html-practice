8d9b8ef296ba2d2073d31eec4237610da28aa15c
ShraddhaSlothe <shraddhalothe01@gmail.com>
2024-01-05 06:37:49 +0530

    practice for the day of 4rth january

---
+++ js/tut53.js
@@ -0,0 +1,10 @@
+console.log("this is tutorial 53");
+
+let name = "shubham";
+let name1 = "saurabh";
+let name2 = "prasad";
+let name3 = "ankush";
+console.log(name +  "is a good boy");
+console.log(name1 +  "is a good boy");
+console.log(name2+  "is a good boy");
+console.log(name3+  "is a  good boy");
